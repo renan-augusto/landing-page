8cc910e05646010a51429391cf63150fbcba6461
renan-augusto <renan_acm1992@hotmail.com>
2024-03-11 07:20:05 -0300

    initial commit

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, signal } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
+import { sign } from 'crypto';
+
+@Component({
+  selector: 'newsletter-form',
+  standalone: true,
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [
+    NewsletterService
+  ],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss'
+})
+export class NewsletterFormComponent {
+  newsLetterForm: FormGroup;
+  loading = signal(false);
+
+  constructor(private service: NewsletterService) {
+    this.newsLetterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit() {
+    this.loading.set(true);
+    if(this.newsLetterForm.valid) {
+      this.service.sendData(this.newsLetterForm.value.name, this.newsLetterForm.value.email).subscribe({
+        next: () => {
+          this.newsLetterForm.reset();
+          this.loading.set(false);
+        }
+      })
+    }
+  }
+
+}
